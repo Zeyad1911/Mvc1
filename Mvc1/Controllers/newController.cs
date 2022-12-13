688a6340696ea1aadc8b780f666960ee4a1c678b
z1 <z1@DESKTOP-OINS26C>
2022-12-13 20:14:42 +0200

    Add project files.

---
+++ Mvc1/Controllers/newController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc1.Controllers
+{
+    public class newController : Controller
+    {
+        // GET: new
+        public ActionResult Index()
+        {
+            AdventureWorks2017Entities a = new AdventureWorks2017Entities();
+            var item = a.People.ToList();
+            return View(item);
+        }
+    }
+}
